0cdfcb499ee2a9aa49d1bc95f5afa5942d93de2d
Pathan Samina <100022106+pathan-samina@users.noreply.github.com>
2024-01-08 09:55:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  // Use "output: export" configuration
-  output: 'export',
 };
 
 module.exports = nextConfig;
